57f68dc1755a1fe1fd18acb89168fe877c39dba3
Joaoof <joaodeus400@gmail.com>
2023-03-13 08:52:48 -0300

    Commit: Estrutura da aplicação - Configurando ESLint

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,10 @@ app.get('/hello', () => {
   return 'Hello Node'
 })
 
-app.listen({
-  port: 3335,
-}).then(() => {
-  console.log('HTTP Server Running!')
-})
+app
+  .listen({
+    port: 3335,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
