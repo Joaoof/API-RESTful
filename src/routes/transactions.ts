38ab20e8bfe85150e7f070dbf8c9b730c049250b
Joaoof <joaodeus400@gmail.com>
2023-03-18 14:19:20 -0300

    Configurando um hook global

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,19 +5,23 @@ import { knex } from '../database'
 import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  // app.addHook('preHandler', async (request, reply) => {
+  //   console.log(`[${request.method}] ${request.url}`)
+  // })
+
   app.get(
     '/',
     {
       preHandler: [checkSessionIdExists],
     },
-    async (request, reply) => {
+    async (request) => {
       const { sessionId } = request.cookies
 
-      if (!sessionId) {
-        return reply.status(401).send({
-          error: 'Unauthorized',
-        })
-      }
+      // if (!sessionId) {
+      //   return reply.status(401).send({
+      //     error: 'Unauthorized',
+      //   })
+      // }
 
       const transactions = await knex('transactions')
         .where('session_id', sessionId)
